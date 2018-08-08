386a9d704b5d047df0555eba8fe4af89992d56b5
Juan Tenezaca <tenj34@gmail.com>
2018-08-08 16:59:53 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,11 +13,4 @@ class Artist
     song.artist = self
   end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
 end
