77fa9f43d3a7b58580b4a75633929c609f6c74da
Juan Tenezaca <tenj34@gmail.com>
2018-08-08 17:35:04 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,7 +7,7 @@ class Author
   end
 
   def add_post(post) # takes an arguement of a post
-    post.author = self # associates with the author by telling the post that it belongs to the authro
+    post.author = self # associates with the author by telling the post that it belongs to the author
 
   end
 
